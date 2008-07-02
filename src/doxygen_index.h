73ec68d506077e40e20b58170223c58a6b71d226
Oliver Bock <oliver.bock@aei.mpg.de>
2008-07-02 18:30:43 +0200

    Extended documentation
    * Added a "header" which holds the index page's content (to be continued...)
    * Fixed excludes
    * Disabled the doxygen tree view (ugly and no advantage)

---
+++ src/doxygen_index.h
@@ -0,0 +1,20 @@
+/*! \mainpage Einstein@Home Graphics Application
+ *
+ * \section cha_intro Introduction
+ *
+ * This is the introduction.
+ *
+ * \section cha_app The Graphics Application
+ *
+ * \subsection sec_app_overview Overview
+ * \subsection sec_app_usage Usage
+ * \subsection sec_app_install Installation
+ *
+ * \section cha_frame The Framework
+ * \subsection sec_frame_overview Overview
+ * \subsection sec_frame_usage Usage
+ *
+ * \section cha_orc The Open Resource Compiler
+ * \subsection sec_orc_overview Overview
+ * \subsection sec_orc_usage Usage
+ */
